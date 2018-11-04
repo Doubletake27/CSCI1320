9e663e851fe78b080f4a74177e091a5820a6ab13
Doubletake27 <henry.meyerson@gmail.com>
2018-11-04 15:19:24 -0700

    Friday Notes

---
+++ C++ Lecture Notes/randGen.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include<cmath>
+#include<ctime>
+
+int main(){
+  int d = 3;
+  //cout << time(NULL) << " seconds has passed since 00:00:00 GMT, Jan 1 1970" << endl;
+  srand(time(NULL)
+);
+  for(int i = 0; i < 12; i++){
+    cout << rand()%(d+1) <<" ";
+  }
+  cout << endl;
+  return 0;
+}
